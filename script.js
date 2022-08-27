15b3f542b54c2738e0b9dc990dd54d721d6e29b8
Ryan Sch <ryan@Ryans-MacBook-Air.local>
2022-08-27 10:32:06 -0400

    Added games back function

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-let firstWins = document.querySelector('.first-wins');
-let firstLoses = document.querySelector('.first-loses');
-let firstTotalGames = parseInt(firstLoses.textContent) + parseInt(firstWins.textContent);
-console.log(firstTotalGames);
-let wins = document.querySelectorAll('.wins');
-let loses = document.querySelectorAll('.loses');
-wins.forEach((win) => {
-	let gamesBack = firstWins.textContent - win.textContent;
+let firstWins = document.querySelector('.first-wins').textContent;
+let firstLoses = document.querySelector('.first-loses').textContent;
+let firstGameDifferential = parseInt(firstWins) - parseInt(firstLoses);
+const non1Teams = Array.from(document.querySelectorAll('.team'));
+let wins = Array.from(document.querySelectorAll('.wins'));
+let loses = Array.from(document.querySelectorAll('.loses'));
+let gamesBackArea = Array.from(document.querySelectorAll('.games-back'));
+
+function Team(wins, loses) {
+	this.wins = wins;
+	this.loses = loses;
+	let teamDifferential = parseInt(wins) - parseInt(loses);
+	this.gamesBackNumber = function() {
+		let gamesBackNumber = (firstGameDifferential - teamDifferential) / 2;
+		return gamesBackNumber;
+ }
+}
+
+let teamNumber = 0;
+
+non1Teams.forEach(() => {
+	let newTeam = new Team(wins[teamNumber].textContent, loses[teamNumber].textContent);
+	let TeamGamesBack = newTeam.gamesBackNumber();
+	let TeamGamesBackArea = gamesBackArea[teamNumber];
+	TeamGamesBackArea.textContent = TeamGamesBack;
+	teamNumber += 1;
 })
+
+
